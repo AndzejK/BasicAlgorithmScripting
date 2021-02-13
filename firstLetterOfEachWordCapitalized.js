30a046ded4233be71ac42fd9cb053fba256e4b19
MAC <andrejkrupoves@gmail.com>
2021-02-14 08:07:34 +1100

    the 1st letter of each word is capitalised
    I was able to solve this problem on 2nd day. The task was taken from www.freecodecamp.org

--- firstLetterOfEachWordCapitalized.js
+++ firstLetterOfEachWordCapitalized.js
@@ -1,21 +1,22 @@
 //toUpperCase();
 //toLowerCase();
-let someTxt="I'm a little tea pot";
+let someTxt="I'm a lit-tle tea pot";
 
 function titleCase(str) {
     let allLettersLowerCase="";
-    let regMachWord=/\w*\S/ig;
+    let regMachWord=/\w*['-]?\w*\S/ig; // don't know how to exclude some symbols, for intance if we're gonna have a comma.
     let arrSepWords=[];
     let transformedSentence="";
     allLettersLowerCase=str.toLowerCase();
     arrSepWords=allLettersLowerCase.match(regMachWord);
+    let getLastItemInArr=arrSepWords[arrSepWords.length-1];
+    let lastIndex=arrSepWords.indexOf(getLastItemInArr);
    for(let i=0;i<arrSepWords.length;i++){
         transformedSentence+=arrSepWords[i].slice(0,1).toUpperCase();
         transformedSentence+=arrSepWords[i].slice(1,arrSepWords[i].length);
-        if(arrSepWords[arrSepWords.length]!=arrSepWords[i]){
-            transformedSentence+=" ";
-        }
-        
+            if(lastIndex!=i){
+             transformedSentence+=" ";
+            }
        }
     return console.log(transformedSentence);
   }

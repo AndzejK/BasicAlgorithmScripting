6befc54d124ad8c533493369f9cc93c8c4dbde97
MAC <andrejkrupoves@gmail.com>
2021-02-17 07:37:39 +1100

    Delete items from an array

--- removeSomeValueFromArray.js
+++ removeSomeValueFromArray.js
@@ -1,9 +1,9 @@
 /*
     Task: Remove all falsy values from an array.
-Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+    Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 */
 
-let someArr=[7, "ate", "", false, 9];
+let someArr=[false, "I'm a big guy", null, 0, NaN, undefined, ""];
 function bouncer (arr){
     /*
         The plan to execute:
@@ -13,20 +13,19 @@ function bouncer (arr){
     */
    let arrWithBooleanValues=[];
    let modifiedArr=[];
-   
+   //Using Boolean method to convert all items into boolean values;
    for(let i=0;i<arr.length;i++){
-      arrWithBooleanValues.push(Boolean(arr[i]));
-   } 
-   for(let i=0; i<arr.length;i++){
-       if(arrWithBooleanValues[i]==true){
-           
-       } else{
-        arr.splice(i,1);
-       }
+      //arrWithBooleanValues.push(Boolean(arr[i]));
+      if(arr[i]){ //
+        modifiedArr.push(arr[i]);
+       } /*else{
+        modifiedArr[i]='delete me please!!';
+   } */
    }
    
-   console.log(arr);
-    return console.log(arrWithBooleanValues)
+   //console.log(arr);
+    //console.log(modifiedArr);
+    return console.log(modifiedArr)
 }
 
 bouncer(someArr)
